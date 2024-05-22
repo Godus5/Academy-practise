dc9de1895c3004ca9ccfefa11b3b28914fa9a9b1
Anton Danielovich <antonhashabov@gmail.com>
2024-05-22 15:32:41 +0300

    Initial commit

---
+++ lesson-6/LabReports/app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :lab_reports
+
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }, length: { maximum: 150 }
+  validates :first_name, presence: true, length: { maximum: 100 }
+  validates :last_name, presence: true, length: { maximum: 100 }
+end
