f478e51cfb85102e0edd9965c75db69d0e8d1502
Anton Danielovich <antonhashabov@gmail.com>
2024-05-23 15:28:54 +0300

    Initital commit

---
+++ lesson-6/LabReports/app/models/lab_report.rb
@@ -0,0 +1,8 @@
+class LabReport < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 500 }
+  validates :description, length: { maximum: 250 }
+  validates :grade, inclusion: { in: %w(A B C D E FX F None),
+    message: "%{value} is not a valid grade" }
+end
