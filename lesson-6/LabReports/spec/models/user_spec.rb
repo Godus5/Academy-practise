d0c148479eb3a531921828e18efa9602c2615489
Anton Danielovich <antonhashabov@gmail.com>
2024-05-27 12:43:38 +0300

    Creating tests for the user model

---
+++ lesson-6/LabReports/spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'Validations' do
+    subject { User.new(email:, first_name:, last_name:).valid? }
+
+    context 'Creating a new record with valid attributes' do
+      let(:email) { 'example@example.com' }
+      let(:first_name) { 'first_name' }
+      let(:last_name) { 'last_name' }
+
+      it 'The check runs without errors' do
+        expect(subject).to eq(true)
+      end
+    end
+
+    context 'Creating a new record with invalid attributes' do
+      let(:email) { 'example@example' }
+      let(:first_name) { 'first_name' }
+      let(:last_name) { 'last_name' }
+
+      it 'The check runs without errors' do
+        expect(subject).to eq(false)
+      end
+    end
+  end
+end
